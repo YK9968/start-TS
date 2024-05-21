70b6b9a0511fb14fc43f601eb43e842c90a5578a
yurii <robka4an@gmail.com>
2024-05-21 14:27:20 +0300

    add file

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ const age: number = 50;
 const username: string = "Max";
 const toggle: boolean = true;
 const empty: null = null;
-const callback = (a: any) => {
+const callback = (a: number) => {
   return 100 + a;
 };
 
@@ -77,6 +77,13 @@ function customError(): never {
 
 // ============================================
 
+// Завдання 5
+
+// Виконуйте це завдання у файлі src/basic/5.ts.
+
+// Типізуйте функцію isWeekend яка приймає день тижня з enumDayOfWeek і повертає
+// boolean значення, що вказує, чи це день робочий чи вихідний.
+
 enum DayOfWeek {
   Monday,
   Tuesday,
@@ -90,3 +97,55 @@ enum DayOfWeek {
 const isWeekend = (day) => {};
 
 // ===================================
+// Завдання 6
+
+// Виконуйте це завдання у файлі src/basic/6.ts.
+
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості.
+// Зверніть увагу, що адреса є необов'язковою властивістю.
+
+const mango = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
+
+// =========================================================
+
+// Завдання 7
+
+// Виконуйте це завдання у файлі src/basic/7.ts.
+
+// У вас є два об'єкти:
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
+
+// Створіть новий тип даних, який підходить для цих двох об'єктів.
+
+// ==============================================================================
